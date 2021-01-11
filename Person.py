b88a4ad509c66b20eb1c462298cdbaebfa11b177
magluva-code <77292885+magluva-code@users.noreply.github.com>
2021-01-11 19:14:16 +0100

    Very rough initial blueprint for a person
    Not sure if this will be kept

---
+++ Person.py
@@ -0,0 +1,41 @@
+# Class Person
+
+class Person(object):
+    """
+    @constant STATUSES (list)
+    An instance of Person is a part of the total population size N.
+    Person p can have status as susceptible, Infected, Deceased, Recovered or Vaccinated.
+    """
+    def __init__(self, status="S"):
+        # Needs custom exception.
+        self.STATES = ["S", "I", "D", "R", "V"]
+        if status not in self.STATES:
+            raise Exception
+        self.status = status
+        self.symptomatic = False
+        self.infectious = False
+        self.quarantined = False
+        self.history = {0 : self.status}
+
+    def set_status(self, status, t):
+        """
+        @param: status (str), t (int)
+        @return: void
+        Change self.status and update self.history.
+        """
+        # Needs custom exception.
+        if status not in self.STATES:
+            raise Exception
+        self.status = status
+        self.update_history(t)
+
+    def update_history(self, t):
+        """
+        @param: t (int)
+        @return: void
+        Update self.history.
+        """
+        self.history[t] = self.status
+
+    def __call__(self):
+        return self.status
